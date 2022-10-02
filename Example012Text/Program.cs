404b9416f982c9f7bf7a88889f8e91c0cdfbdaac
Sofia Kabanova <sofia.kabanova@gmail.com>
2022-10-02 23:01:06 +0300

    done examples 012

---
+++ Example012Text/Program.cs
@@ -0,0 +1,34 @@
+﻿//=====Работа с текстом
+// Дан текст. В тексте нужно все пробелы заменить чёрточками, // маленькие буквы “к” заменить большими “К”,
+// а большие “С” маленькими “с”.
+
+// Ясна ли задача?
+
+string text = "— Я думаю, — сказал князь, улыбаясь, — что,”"
+            + "ежели бы вас послали вместо нашего милого Винценгероде,"
+            + "вы бы взяли приступом согласие прусского короля."
+            + "Вы так красноречивы. Вы дадите мне чаю?";
+// string s = “qwerty”
+//             012345
+//s[3]//r
+
+string Replace(string text, char oldValue, char newValue) 
+{
+    string result = String.Empty; 
+    int length = text.Length;
+    for(int i = 0; i < length; i++)
+    {
+        if(text[i] == oldValue) result = result + $"{newValue}";
+        else result = result + $"{text[i]}"; 
+    }
+    return result;
+}
+
+string newText = Replace(text, ' ', '|'); 
+Console.WriteLine(newText);
+Console.WriteLine();
+newText = Replace(text, 'к', 'К'); 
+Console.WriteLine(newText);
+Console.WriteLine();
+newText = Replace(text, 'а', 'А'); 
+Console.WriteLine(newText);
---
+++ Example012cycleFor/Program.cs
@@ -0,0 +1,42 @@
+﻿// цикл while
+// string Method4(int count, string text)// строку text компануем count раз
+// {
+   // int i=0;
+    // string result=String.Empty; // изначально пустая строка 
+    // while (i<count)
+    // {
+       // result=result+text;
+       // i++;
+    // }
+    // return result;
+// }
+// string res = Method4(10, "текст4");
+// Console.WriteLine(res);
+
+// Переписываем в цикл for: 
+
+// цикл for
+string Method4(int count, string text)// строку text компануем count раз
+{
+    string result=String.Empty; // изначально пустая строка 
+    for (int i = 0; i < count; i++) 
+    // инициализация счетчика; проверка счетчика; инкремент
+    {
+        result=result+text;
+    }
+    return result;
+}
+string res = Method4(10, "текст4");
+Console.WriteLine(res);
+
+// Цикл в цикле.
+// Пример: таблица умножения
+
+for (int i = 0; i <= 10; i++)
+{
+    for (int j = 2; j <= 10; j++)
+    {
+        Console.WriteLine($"{i} * {j} = {i * j}");
+    }
+    Console.WriteLine();
+}
